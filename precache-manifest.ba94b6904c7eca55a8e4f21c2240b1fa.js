bc19dde161f0310e59a1bd7edfef21ae4c12d127
Mark Mbugua <mbuguamark@gmail.com>
2019-07-12 10:44:38 +0300

    Updates

---
+++ precache-manifest.ba94b6904c7eca55a8e4f21c2240b1fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e0e6cb27ad1e1f7eef7503720c11e74",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "908ba66d059eb83d7a96",
+    "url": "/weather-app/static/css/main.5ebe3222.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/weather-app/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "908ba66d059eb83d7a96",
+    "url": "/weather-app/static/js/main.cabb4bd9.chunk.js"
+  },
+  {
+    "revision": "90e6a6c358468ae00209",
+    "url": "/weather-app/static/js/runtime~main.4606d7f5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/weather-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
